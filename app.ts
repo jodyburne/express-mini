4b8093a9864a7d176a4dece9da451999a0fce131
jodyburne <j.richardson@stuart.com>
2022-09-21 19:14:49 +0100

    adding wedpack hot middleware

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
+import webpack from "webpack";
+import webpackConfig from "./webpack.config";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+
+const compiler = webpack(webpackConfig);
+
 const path = require("path");
 
 const app = express();
 const port = 3009;
 app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.use(webpackDevMiddleware(compiler, {}));
+app.use(webpackHotMiddleware(compiler, {}));
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
