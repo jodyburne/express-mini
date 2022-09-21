92a258b391ed5725298cbefdf3e9b98b2bfa6a7c
jodyburne <j.richardson@stuart.com>
2022-09-21 15:28:34 +0100

    added webpack config

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
+const path = require("path");
+
 const app = express();
 const port = 3009;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hi hunni");
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
